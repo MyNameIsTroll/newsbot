35567832073dd48d59084ae420daabd7231fc31d
MyNameIsTroll <31790025+MyNameIsTroll@users.noreply.github.com>
2024-01-23 21:34:54 +0000

    Update newsbot.py

--- newsbot.py
+++ newsbot.py
@@ -4,39 +4,39 @@ import json
 from datetime import datetime, timedelta
 from bs4 import BeautifulSoup
 
-# Créez une nouvelle instance de l'API Mastodon dans /settings/applications et copiez vos identifiants.
+# Create a new instance of the Mastodon API in /settings/applications (+ new application) and copy your credentials.
 mastodon = Mastodon(
     client_id='xxx',
     client_secret='xxx',
     access_token='xxx',
-    api_base_url='https://xxx.tld'  # Remplacez par l'URL de votre instance
+    api_base_url='https://xxx.tld'  # Replace with the URL of your instance
 )
 
-# Lire les flux RSS à partir d'un fichier texte
-with open('/HOME/USER/PATH/rss_feeds.txt', 'r') as f: # Modifiez par votre chemin
+# Read RSS feeds from a text file
+with open('/HOME/USER/PATH/rss_feeds.txt', 'r') as f: # Modify your path
     rss_feeds = f.readlines()
 
-# Lire les liens déjà publiés à partir d'un fichier json
+# Read links already published from a json file
 try:
-    with open('/HOME/USER/PATH/published_links.json', 'r') as f: # Modifiez par votre chemin
+    with open('/HOME/USER/PATH/published_links.json', 'r') as f: # Modify your path
         published_links = json.load(f)
 except FileNotFoundError:
     published_links = []
 
-# Lire la dernière fois que le cache a été vidé à partir d'un fichier
+# Read the last time the cache was cleared from a file
 try:
-    with open('/home/pi/mes_bots/rts_bot/last_cache_clear.txt', 'r') as f:
-        last_cache_clear_str = f.read().strip()  # Supprime les espaces blancs au début et à la fin
-        if last_cache_clear_str:  # Vérifie si la chaîne n'est pas vide
+    with open('/HOME/USER/PATH/last_cache_clear.txt', 'r') as f: # Modify your path
+        last_cache_clear_str = f.read().strip()  # Removes leading and trailing white space
+        if last_cache_clear_str: # Checks if the string is not empty
             last_cache_clear = datetime.strptime(last_cache_clear_str, "%Y-%m-%d %H:%M:%S.%f")
         else:
             last_cache_clear = datetime.now()
 except FileNotFoundError:
     last_cache_clear = datetime.now()
 
-post_limit = 3  # Limite du nombre de toots par exécution du script (défaut = 3)
-time_limit = datetime.now() - timedelta(days=1)  # Limite de temps pour les articles du flux RSS, poste uniquement les nouvelles entrées datant de moins d'un jour.
-post_count = 0  # Compteur du nombre de toots
+post_limit = 3  # Limit number of toots per script execution (default = 3)
+time_limit = datetime.now() - timedelta(days=1)  # Time limit for RSS feed articles, post only new entries less than one day old.
+post_count = 0  # Toots counter, do not modify
 
 for feed in rss_feeds:
     d = feedparser.parse(feed)
@@ -45,7 +45,7 @@ for feed in rss_feeds:
             break
         published_time = datetime.strptime(entry.published, "%a, %d %b %Y %H:%M:%S %Z")
         if entry.link not in published_links and published_time > time_limit:
-            # Format du post
+            # Publishing format
             soup = BeautifulSoup(entry.summary, "html.parser")
             clean_summary = soup.get_text()
             post = f"{entry.title}\n{entry.link}\n{clean_summary}"
@@ -55,14 +55,14 @@ for feed in rss_feeds:
             published_links.append(entry.link)
             post_count += 1
 
-# Enregistrer les liens déjà publiés dans un fichier json
-with open('/home/pi/mes_bots/rts_bot/published_links.json', 'w') as f:
+# Save previously published links in a json file
+with open('/HOME/USER/PATH/published_links.json', 'w') as f: # Modify your path
     json.dump(published_links, f)
 
-# Vider le cache chaque semaine
+# Empty cache weekly
 if datetime.now() - last_cache_clear > timedelta(weeks=1):
     published_links = []
     last_cache_clear = datetime.now()
-    # Enregistrer la dernière fois que le cache a été vidé dans un fichier
-    with open('/home/pi/mes_bots/rts_bot/last_cache_clear.txt', 'w') as f:
+    # Save the last time the cache was cleared in a file
+    with open('/HOME/USER/PATH/last_cache_clear.txt', 'w') as f:
         f.write(last_cache_clear.strftime("%Y-%m-%d %H:%M:%S.%f"))
